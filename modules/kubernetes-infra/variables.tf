fb175745435af44d4161d39139a90a3e335b9ed6
Martin Roberts <temples_spyware0c@icloud.com>
2022-08-27 16:10:52 -0400

    Fixing the terraform

--- modules/kubernetes-infra/variables.tf
+++ modules/kubernetes-infra/variables.tf
@@ -1,21 +1,17 @@
 variable "cluster_name" {
   description = "Value of the Kubernetes Cluster Name"
   type        = string
-  nullable = false
 }
 variable "region" {
   description = "Which region to use for cloud infrastructure"
   type        = string
-  nullable = false
 }
 
 variable "vpc_id" {
   description = "The VPC ID to use"
-  nullable = false
 }
 
 variable "subnet_ids" {
   description = "The Subnet IDs to use"
-  nullable = false
 }
 
