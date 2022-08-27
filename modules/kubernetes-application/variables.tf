05c3a2c59ab7b96d385387abba4aa7f44076cb7a
Martin Roberts <temples_spyware0c@icloud.com>
2022-08-27 16:22:51 -0400

    Revert "Merge pull request #2 from martin40701/terraform"
    This reverts commit 50fe7b85c89430ddb769b8596addefa81518c1e1, reversing
    changes made to f891f3a9002e936af3e1caffde8255a12c86f819.

--- modules/kubernetes-application/variables.tf
+++ modules/kubernetes-application/variables.tf
@@ -1,18 +1,23 @@
 variable "cluster_name" {
   description = "Value of the Kubernetes Cluster Name"
   type        = string
+  nullable = false
 }
 variable "region" {
   description = "Which region to use for cloud infrastructure"
   type        = string
+  nullable = false
 }
 variable "cluster_id" {
   description = "Value of the Kubernetes Cluster ID"
+  nullable = false
 }
 variable "endpoint" {
   description = "Value of the Kubernetes Cluster Endpoint"
+  nullable = false
 }
 
 variable "eks_ca_data" {
   description = "Value of the Kubernetes Cluster CA Data"
+  nullable = false
 }
